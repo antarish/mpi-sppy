a432e7daf7984a2954ae9024fc74f5466d5225c7
Ant <antarish22@gmail.com>
2024-08-15 10:57:12 -0700

    Changed trials to 10 in mygo.bash

--- mpisppy/agnostic/examples/mygo.bash
+++ mpisppy/agnostic/examples/mygo.bash
@@ -5,4 +5,4 @@ ampl_data_file="steel.dat"
 #python -u ../agnostic_cylinders.py --module-name mpisppy.agnostic.examples.steel_ampl_model --default-rho 1 --num-scens 3 --seed 17 --solver-name gurobi --guest-language AMPL --ampl-model-file steel.mod --ampl-data-file "$ampl_data_file" --max-iterations 10
 #could i ad a -- data file name 
 
-mpiexec -np 3 python -u ../agnostic_cylinders.py --module-name mpisppy.agnostic.examples.steel_ampl_model --default-rho 1 --num-scens 3 --seed 17 --solver-name gurobi --guest-language AMPL --ampl-model-file steel.mod --ampl-data-file "$ampl_data_file" --max-iterations 1 --xhatshuffle --lagrangian --write-solution
+mpiexec -np 3 python -u ../agnostic_cylinders.py --module-name mpisppy.agnostic.examples.steel_ampl_model --default-rho 1 --num-scens 3 --seed 17 --solver-name gurobi --guest-language AMPL --ampl-model-file steel.mod --ampl-data-file "$ampl_data_file" --max-iterations 10 --xhatshuffle --lagrangian --write-solution
